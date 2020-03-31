d5eb1adf6da998534a0e358d442fba95e6c1e41c
Brian Bat-Enkh <51034241+shijir0927@users.noreply.github.com>
2020-03-30 19:28:38 -0700

    js

---
+++ js/recall.js
@@ -0,0 +1,9 @@
+const source = document.getElementById("source");
+const result = document.getElementById("result");
+
+const inputHandler = function(e) {
+  result.innerHTML = e.target.value;
+};
+
+source.addEventListener("input", inputHandler);
+source.addEventListener("propertychange", inputHandler);
